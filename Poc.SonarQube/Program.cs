9d14bef2cb60958c3307f8b75491d476cd8eca25
SzajowsP <piotr.szajowski@brightmls.com>
2020-04-03 10:11:46 -0400

    init commit

---
+++ Poc.SonarQube/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using System.Threading;
+
+namespace Poc.SonarQube
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string a;
+            int b;
+            var c = true;
+            if (c == true)
+            {
+
+            }
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
