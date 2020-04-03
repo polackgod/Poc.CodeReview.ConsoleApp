6eef7ed495b4e0e087700a057d915480dd6f6347
SzajowsP <piotr.szajowski@brightmls.com>
2020-04-03 11:06:56 -0400

    adding bad stuff back in

--- Poc.SonarQube/Program.cs
+++ Poc.SonarQube/Program.cs
@@ -9,7 +9,13 @@ namespace Poc.SonarQube
     {
         static void Main(string[] args)
         {
+            string a;
+            int b;
+            var c = true;
+            if (c == true)
+            {
 
+            }
             Console.WriteLine("Hello World!");
         }
 
